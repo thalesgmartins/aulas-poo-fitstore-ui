bc36ae7e41c92d70d454da6d426d6ca5846843d2
thalesgmartins <thalesdiniz30@gmail.com>
2025-12-09 01:18:22 -0300

    att

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { Principal } from './principal/principal';
 import { Cadastro } from './cadastro/cadastro';
+import { Login } from './login/login';
 
 export const routes: Routes = [
     {
@@ -10,5 +11,9 @@ export const routes: Routes = [
     {
         path: 'cadastro',
         component: Cadastro
+    },
+    {
+        path: 'login',
+        component: Login
     }
 ];
--- src/app/leiaute/header/header.ts
+++ src/app/leiaute/header/header.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { AsyncPipe, CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from "@angular/router";
+import { UsuarioService } from '../../usuario-service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule, AsyncPipe],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
+  private usuarioService = inject(UsuarioService);
+
+  public isLoggedIn$: Observable<boolean> = this.usuarioService.isLoggedIn$;
+
+  logout() {
+    this.usuarioService.logout();
+  }
 
 }
