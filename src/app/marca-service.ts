a08ad3ff23dc09ac45158b29e6b305a204394bba
thalesgmartins <thalesdiniz30@gmail.com>
2025-12-10 00:28:44 -0300

    add service da marca

---
+++ src/app/marca-service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MarcaService {
+  private http = inject(HttpClient);
+  
+  listar(): Observable<any[]> {
+    return this.http.get<any[]>('http://localhost:8080/marcas');
+  }
+
+}
