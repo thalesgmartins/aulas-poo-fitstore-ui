a7ce7bfe672ee3daae98de19d35f6328d708ef30
thalesgmartins <thalesdiniz30@gmail.com>
2025-12-10 01:25:16 -0300

    add operações do service

--- src/app/produto-service.ts
+++ src/app/produto-service.ts
@@ -7,8 +7,20 @@ import { Observable } from 'rxjs';
 })
 export class ProdutoService {
   private http = inject(HttpClient);
+  private apiUrl = 'http://localhost:8080/produtos';
   
   listar(): Observable<any[]> {
-    return this.http.get<any[]>('http://localhost:8080/produtos');
+    return this.http.get<any[]>(this.apiUrl);
+  }
+
+  salvar(produto: any): Observable<any> {
+    return this.http.post(this.apiUrl, produto);
+  }
+
+  // Método para enviar a foto antes de salvar o produto
+  uploadImagem(file: File): Observable<{ uuid: string }> {
+    const formData = new FormData();
+    formData.append('file', file);
+    return this.http.post<{ uuid: string }>(`${this.apiUrl}/upload`, formData);
   }
 }
