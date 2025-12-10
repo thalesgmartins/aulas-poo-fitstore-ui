2f9a87a3d23f3a54fa5a2d8023b6ac4d87a33713
thalesgmartins <thalesdiniz30@gmail.com>
2025-12-10 00:30:53 -0300

    checagem do status da conexão

--- src/app/usuario-service.ts
+++ src/app/usuario-service.ts
@@ -2,17 +2,41 @@ import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable, throwError } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
 import { Injectable, inject } from '@angular/core';
+import { Router } from "@angular/router";
 
 @Injectable({
   providedIn: 'root',
 })
 export class UsuarioService {
-  private readonly http = inject(HttpClient);
   private API_URL = 'http://localhost:8080';
+
+  // Injeta o cliente http para fazer requisições
+  private readonly http = inject(HttpClient);
+  private router = inject(Router);
+
+  // Armazena o perfil do usuário
+  private _userRole = new BehaviorSubject<string | null>(null);
+  public userRole$ = this._userRole.asObservable();
 
   private _isLoggedIn = new BehaviorSubject<boolean>(false);
   public isLoggedIn$ = this._isLoggedIn.asObservable();
   
+  checkAuthStatus(): Observable<AuthStatusResponse> {
+    return this.http.get<AuthStatusResponse>(`${this.API_URL}/usuarios/status`, {
+      withCredentials: true
+    }).pipe(
+      tap((response: AuthStatusResponse) => {
+        this._isLoggedIn.next(true);
+        this._userRole.next(response.perfil);
+      }),
+      catchError((err) => {
+        this._isLoggedIn.next(false);
+        this._userRole.next(null);
+        return throwError(() => err);
+      })
+    )
+  }
+
   criarUsuario(usuario: any): Observable<any> {
     return this.http.post<any>('http://localhost:8080/usuarios', usuario);
   }
@@ -22,9 +46,10 @@ export class UsuarioService {
       responseType: 'text',
       withCredentials: true
     }).pipe(
-      tap(() => this._isLoggedIn.next(true)),
+      tap(() => this.checkAuthStatus().subscribe()),
         catchError((err) => {
         this._isLoggedIn.next(false);
+        this._userRole.next(null);
         return throwError(() => err)
       })
     );
@@ -55,7 +80,7 @@ export class UsuarioService {
         }),
         catchError((err) => {
             // Se o logout falhar (o cookie já expirou ou foi inválido), ainda assim limpe o estado local
-    this._isLoggedIn.next(false);
+            this._isLoggedIn.next(false);
             this._userRole.next(null);
             // Não precisa de throwError aqui, pois o logout deve sempre tentar limpar o estado local
             return err;
