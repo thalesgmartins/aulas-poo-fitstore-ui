d2879a8c1d729da7e7ef8b76840f115fcf167a86
thalesgmartins <thalesdiniz30@gmail.com>
2025-12-10 01:44:31 -0300

    carrega a filtragem de produtos

--- src/app/principal/principal.ts
+++ src/app/principal/principal.ts
@@ -1,22 +1,45 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ProdutoService } from '../produto-service';
+import { combineLatest } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-principal',
-  imports: [],
+  imports: [ CommonModule],
   templateUrl: './principal.html',
   styleUrl: './principal.css',
 })
 export class Principal implements OnInit {
   private produtoService = inject(ProdutoService);
 
+  todosProdutos: any[] = []; // Guarda a cópia original completa
+  produtosFiltrados: any[] = []; // O que é exibido na tela
+
   public listaProdutos: any[] = [];
 
-  ngOnInit(): void {
-    this.produtoService.listar().subscribe({
-      next: (produtos) => {
-        this.listaProdutos = produtos;
+  ngOnInit() {
+    // 1. Carrega os produtos e se inscreve nas mudanças de filtro
+    // combineLatest: Espera os dois (lista e filtros) estarem prontos
+    combineLatest([
+      this.produtoService.listar(),
+      this.produtoService.filtros$
+    ]).subscribe(([produtos, filtros]) => {
+      
+      // Atualiza a lista original (apenas na primeira carga ou refresh)
+      if (this.todosProdutos.length === 0) { 
+         this.todosProdutos = produtos;
       }
+
+      // 2. Aplica a Lógica de Filtragem
+      this.produtosFiltrados = this.todosProdutos.filter(produto => {
+        // Verifica Marca (se houver alguma selecionada)
+        const passaMarca = filtros.marcas.length === 0 || filtros.marcas.includes(produto.marca?.id);
+        
+        // Verifica Categoria (se houver alguma selecionada)
+        const passaCategoria = filtros.categorias.length === 0 || filtros.categorias.includes(produto.categoria?.id);
+
+        return passaMarca && passaCategoria;
+      });
     });
   }
 }
