5d037f6e6220cd06bc2e3ecd10abd96eee13b5b9
thalesgmartins <thalesdiniz30@gmail.com>
2025-12-10 01:25:42 -0300

    att operações service

--- src/app/categoria-service.ts
+++ src/app/categoria-service.ts
@@ -7,9 +7,20 @@ import { Observable } from 'rxjs';
 })
 export class CategoriaService {
   private http = inject(HttpClient);
+  private apiUrl = "http://localhost:8080/categorias";
   
   listar(): Observable<any[]> {
-    return this.http.get<any[]>('http://localhost:8080/categorias');
+    return this.http.get<any[]>(this.apiUrl);
+  }
+criar(obj: any): Observable<any> {
+    return this.http.post(this.apiUrl, obj);
   }
 
+  atualizar(id: number, obj: any): Observable<any> {
+    return this.http.put(`${this.apiUrl}/${id}`, obj);
+  }
+
+  deletar(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
 }
