709f292c42243cffcf13de908bde28bef893a87c
thalesgmartins <thalesdiniz30@gmail.com>
2025-12-10 00:31:56 -0300

    mostra as marcas de acordo com o service

--- src/app/leiaute/menu/menu.ts
+++ src/app/leiaute/menu/menu.ts
@@ -1,5 +1,6 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { CategoriaService } from '../../categoria-service';
+import { MarcaService } from '../../marca-service';
 
 @Component({
   selector: 'app-menu',
@@ -8,12 +9,19 @@ import { CategoriaService } from '../../categoria-service';
   styleUrl: './menu.css',
 })
 export class Menu implements OnInit {
-  private service = inject(CategoriaService);
+  // Injeta os seviços no código
+  private categoriaService = inject(CategoriaService);
+  private marcaService = inject(MarcaService);
 
+  // Exporta as categorias e marcas
   public listaCategorias: any[] = [];
+  public listaMarcas: any[] = [];
 
+  // Gancho que roda ao carregar o componente
   ngOnInit(): void {
-    this.service.listar().subscribe({
+
+    // Puxa as Categorias
+    this.categoriaService.listar().subscribe({
       next: (categorias) => {
         this.listaCategorias = categorias;
       },
@@ -21,6 +29,15 @@ export class Menu implements OnInit {
         console.error('Erro ao carregar categorias:', erro);
       }
     });
-  }
 
+    // Puxas as Marcas
+    this.marcaService.listar().subscribe({
+      next: (marcas) => {
+        this.listaMarcas = marcas;
+      },
+      error: (erro) => {
+        console.error('Erro ao carregar categorias', erro);
+      }
+    });
+  }
 }
