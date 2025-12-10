300b7ad21504d27d8d551954af3ec210787dc2c4
thalesgmartins <thalesdiniz30@gmail.com>
2025-12-10 00:28:31 -0300

    mantém o controle da autenticação

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,19 +1,37 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { RouterOutlet, Router, NavigationEnd } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
 import { FormsModule } from '@angular/forms';
 import { Header } from "./leiaute/header/header";
 import { Footer } from "./leiaute/footer/footer";
 import { Menu } from "./leiaute/menu/menu";
-import { Principal } from "./principal/principal";
-import { RouterOutlet } from '@angular/router';
+import { filter, map } from 'rxjs';
+import { UsuarioService } from './usuario-service';
 
 @Component({
   selector: 'app-root',
-  imports: [FormsModule, Header, Footer, Menu, Principal, RouterOutlet],
+  imports: [FormsModule, Header, Footer, Menu, RouterOutlet, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('ecommerce');
 
+  // Injetando coisas
+  router = inject(Router);
+  private usuarioService = inject(UsuarioService);
 
+  showMenu: boolean = true;
+
+  ngOnInit(): void {
+    // Verifica se o menu de navegação deve aparecer ou não
+    this.router.events.pipe(
+      filter((event) => event instanceof NavigationEnd), map(event => event as NavigationEnd)).subscribe((event:NavigationEnd) => {const url = event.urlAfterRedirects; this.showMenu = !url.includes('/login') && !url.includes('/cadastro');});
+  
+    // Carrega autenticação
+    this.usuarioService.checkAuthStatus().subscribe({
+      error: () => console.log('Sessão não encontrada ou expirada.')
+    })
+  }
 }
